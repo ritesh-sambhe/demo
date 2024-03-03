07fdbc35f382ecd496fdd413e2bd5235d76ca7b1
ritesh-sambhe <144586067+ritesh-sambhe@users.noreply.github.com>
2024-03-03 12:29:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,93 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+# Internet Gateway Creation
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+#Public Subnet Creation
+resource "aws_subnet" "mypublic_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "MyPublic_Subnet"
+  }
+}
+
+# Route Table Creation
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "Public-Route-Table"
+  }
+}
+
+# Route Table Subnet Association
+resource "aws_route_table_association" "myroute_subnet_associate" {
+  subnet_id      = aws_subnet.mypublic_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "my_sg" {
+  name   = "Allow all traffic"
+  vpc_id = aws_vpc.myvpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "My-SG-Firewall"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "my_instance" {
+  ami                         = "ami-0d5bf08bc8017c83b"
+  instance_type               = "t2.micro"
+  key_name                    = "riteshkp"
+  subnet_id                   = aws_subnet.mypublic_subnet.id
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Hello-Terraform"
+  }
+}
